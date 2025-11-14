ce241206683efb8765990aeea4f24398ae35f57c
velossoraptor <taliamolsen@gmail.com>
2025-11-14 10:49:05 -0500

    css and error handling done

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,19 @@
 const express = require('express');
 const router = new express.Router();
 const invController = require('../controllers/invController');
+const utilities = require('../utilities/');
+
 
 // Route to build inventory by classification view
-router.get('/type/:classificationId', invController.buildByClassificationId);
+router.get(
+	'/type/:classificationId',
+	utilities.handleErrors(invController.buildByClassificationId)
+);
 
 // Route to get Inventory Detail Page
-router.get('/details/:invId', invController.buildInvDetail);
+router.get(
+	'/details/:invId',
+	utilities.handleErrors(invController.buildInvDetail)
+);
 
 module.exports = router;
