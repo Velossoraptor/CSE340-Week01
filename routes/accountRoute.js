4dbbb87d61a63380ffe830a0b960330427e43477
velossoraptor <taliamolsen@gmail.com>
2025-11-17 11:49:41 -0500

    Process registration works

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,7 +7,10 @@ const utilities = require('../utilities/');
 // Route for login
 router.get('/login', utilities.handleErrors(accountController.buildLogin));
 
-// Route for registration
+// Route for registration form
 router.get('/register', utilities.handleErrors(accountController.buildRegister));
+
+// Route to actually register
+router.post('/register', utilities.handleErrors(accountController.registerAccount));
 
 module.exports = router;
