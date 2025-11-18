63938ed96b92913637834f178f6d76588178262b
velossoraptor <taliamolsen@gmail.com>
2025-11-18 10:54:29 -0500

    Server side validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,14 +3,23 @@ const express = require('express');
 const router = new express.Router();
 const accountController = require('../controllers/accountController');
 const utilities = require('../utilities/');
+const regValidate = require('../utilities/account-validation');
 
 // Route for login
 router.get('/login', utilities.handleErrors(accountController.buildLogin));
 
 // Route for registration form
-router.get('/register', utilities.handleErrors(accountController.buildRegister));
+router.get(
+	'/register',
+	utilities.handleErrors(accountController.buildRegister)
+);
 
 // Route to actually register
-router.post('/register', utilities.handleErrors(accountController.registerAccount));
+router.post(
+	'/register',
+	regValidate.registrationRules(),
+	regValidate.checkRegData,
+	utilities.handleErrors(accountController.registerAccount)
+);
 
 module.exports = router;
