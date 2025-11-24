bdaf5374a39824e1d5dddfa461af64d77df88672
velossoraptor <taliamolsen@gmail.com>
2025-11-24 12:02:39 -0500

    Table Works - Needs CSS Still

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -48,6 +48,9 @@ router.get(
 	utilities.handleErrors(invController.buildInvDetail)
 );
 
+// Route to get inventory items by id in the management page
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
+
 // Route to trigger 500 series error
 router.get('/trigger500', utilities.handleErrors(invController.throwError));
 
